94be346043d5a07617203017d2c067cfe1ed32a8
kengJ <xheiyu@126.com>
2017-01-02 12:50:42 +0000

    Create AdminDao.java

---
+++ src/main/java/com/dao/AdminDao.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import java.util.List;
+import com.model.Admin;
+
+public interface AdminDao {
+
+	List<Admin> FindAdmin();
+	Admin FineAdminById(Integer AdminId);
+	boolean AddAdmin(Admin admin);
+	boolean DelAdmin(Integer AdminId);
+	boolean UpdateAdmin(Admin admin);
+}
